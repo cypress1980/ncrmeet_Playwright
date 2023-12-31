e700f6171db37ae8199de85c75d8d2c1d6e3a7af
Kailash Chander Pathak <you@example.com>
2023-12-31 18:34:54 +0530

    first Commit for Shade

---
+++ tests/browserContext.spec.js
@@ -0,0 +1,48 @@
+import { test } from '@playwright/test';
+
+test('Browser Context example', async ({ browser }) => {
+
+    //First Context
+    const firstContext = await browser.newContext();
+    const firstPage = await firstContext.newPage();
+    await firstPage.goto('https://automationexercise.com/login');
+    await firstPage.locator('input[data-qa="login-email"]').click();
+    await firstPage.locator('input[data-qa="login-email"]').fill('ncrmeet1@yopmail.com');
+    await firstPage.locator('input[data-qa="login-email"]').press('Tab');
+    await firstPage.locator('input[placeholder="Password"]').fill('Test@1234');
+    await firstPage.locator('button[data-qa="login-button"]').click();
+
+    //Open Page in New Tab
+    const secondContext = await browser.newContext();
+    const secondPage = await firstContext.newPage();
+    await secondPage.goto('https://automationexercise.com/login');
+
+    firstPage.bringToFront()
+    await firstPage.locator('//a[@href="/products"]').click()
+
+/*//Second Context --  Open Page in New Browser 
+    const secondContext = await browser.newContext();
+    const secondPage = await secondContext.newPage();
+    await secondPage.goto('https://automationexercise.com/login');
+    await secondPage.locator('input[data-qa="login-email"]').click();
+    await secondPage.locator('input[data-qa="login-email"]').fill('ncrmeet2@yopmail.com');
+    await secondPage.locator('input[data-qa="login-email"]').press('Tab');
+    await secondPage.locator('input[placeholder="Password"]').fill('Test@1234');
+    await secondPage.locator('button[data-qa="login-button"]').click();
+    firstPage.bringToFront()
+    await firstPage.locator('//a[@href="/products"]').click();*/
+
+
+
+    await firstPage.pause();
+    await secondPage.pause();
+    
+   /* //Closed Page
+    await firstPage.close();
+    await secondPage.close();
+
+
+    //Closed Context
+    await firstContext.close();
+    await secondContext.close();*/
+});
