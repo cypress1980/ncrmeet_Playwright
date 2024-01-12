ca49214a240327925e74444587d904406f3bf480
Kailash Chander Pathak <you@example.com>
2024-01-12 16:23:57 +0530

    Small Change

--- playwright.config.js
+++ playwright.config.js
@@ -41,12 +41,12 @@ module.exports = defineConfig({
     /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    },*/
+    },
 
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
--- tests/browserContextOld.spec.js
+++ tests/browserContextOld.spec.js
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test';
 
 test('Browser Context example', async ({ browser }) => {
+
     //First Context
     const firstContext = await browser.newContext();
     const firstPage = await firstContext.newPage();
@@ -10,6 +11,18 @@ test('Browser Context example', async ({ browser }) => {
     await firstPage.locator('input[data-qa="login-email"]').press('Tab');
     await firstPage.locator('input[placeholder="Password"]').fill('Test@1234');
     await firstPage.locator('button[data-qa="login-button"]').click();
+
+    //Open Page in New Tab
+    /*const secondPage = await firstContext.newPage();
+    await secondPage.goto('https://automationexercise.com/login');
+    await secondPage.locator('input[data-qa="login-email"]').click();
+    await secondPage.locator('input[data-qa="login-email"]').fill('ncrmeet2@yopmail.com');
+    await secondPage.locator('input[data-qa="login-email"]').press('Tab');
+    await secondPage.locator('input[placeholder="Password"]').fill('Test@1234');
+    await secondPage.locator('button[data-qa="login-button"]').click();
+
+    //firstPage.bringToFront()
+    await secondPage.locator('//a[@href="/products"]').click()*/
 
 //Second Context --  Open Page in New Browser 
     const secondContext = await browser.newContext();
@@ -30,6 +43,8 @@ test('Browser Context example', async ({ browser }) => {
     await firstPage.close();
     await secondPage.close();
 
+
+    //Closed Context
     await firstContext.close();
     await secondContext.close();*/
 });
